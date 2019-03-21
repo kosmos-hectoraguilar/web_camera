f3c268cf6fd2ae71bf2b160f692018211ece696f
Hector Efren Aguilar <47463539+kosmos-hectoraguilar@users.noreply.github.com>
2019-03-21 17:50:35 -0600

    Se agrega proceso de calculo de brillo

--- src/camera.js
+++ src/camera.js
@@ -1,36 +1,90 @@
-
 import cv from "opencv.js";
+
+const FPS = 10;
 
 let video = document.getElementById("videoInput"); // video is the id of video tag
 video.width = jQuery(window).width();
 video.height = jQuery(window).height();
 let buttonDiv = document.getElementById("buttonDiv");
+let alertText = document.getElementById("alert");
+
 buttonDiv.height = 1200;
 
 navigator.mediaDevices
-  .getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
+  .getUserMedia({
+    video: true, //{ facingMode: { exact: "environment" } },
+    audio: false
+  })
   .then(function(stream) {
     video.srcObject = stream;
     video.play();
-    
+
     let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let cap = new cv.VideoCapture(video);
 
-    const FPS = 30;
+    let { width, height } = stream.getTracks()[0].getSettings();
+    let pointCount = 480 * 360; //video.height * video.width;
+    console.log(`${width}x${height}`);
+
     function processVideo() {
       try {
         let begin = Date.now();
-
+        //val pointMatHsv = Mat(1, 1, CvType.CV_8UC3, hsvColor)
+        // Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4)
         cap.read(src);
-        cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-        let delay = 1000 / FPS - (Date.now() - begin);
-        setTimeout(processVideo, delay);
-      }catch(err){
+
+        let reducedMap = new cv.Mat(480, 360, cv.CV_8UC4);
+        cv.resize(
+          src, // input image
+          reducedMap, // result image
+          reducedMap.size(), // new dimensions
+          0,
+          0,
+          cv.INTER_CUBIC // interpolation method
+        );
+        let brightness = calculateBrightness(reducedMap, pointCount);
+        reducedMap.delete();
+        notifyBrightness(brightness);
+
+        //cv.cvtColor(src, src, cv.COLOR_RGB2HLS, 0);
+      } catch (err) {
         console.log("An error occurred! " + err);
+      } finally {
+        let delay = 300; //1000 / FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
       }
     }
 
     setTimeout(processVideo, 0);
-  }).catch(function(err) {
+  })
+  .catch(function(err) {
     console.log("An error occurred! " + err);
   });
+
+function calculateBrightness(src, pointCount) {
+  let dst = new cv.Mat();
+  let brightness = 0;
+  cv.cvtColor(src, dst, cv.COLOR_RGB2HLS);
+
+  for (let row = 0; row < dst.rows; row++) {
+    let rowMat = dst.row(row);
+    for (let col = 0; col < dst.cols; col++) {
+      brightness = brightness + rowMat.col(col).data[1];
+    }
+  }
+  dst.delete();
+
+  //brightness = brightness / pointCount;
+  brightness = brightness / pointCount; //(480 * 360);
+  return brightness;
+}
+
+function notifyBrightness(brightness) {
+  if (brightness < 80) {
+    alertText.textContent = "Muy obscuro: " + brightness;
+  } else if (brightness > 200) {
+    alertText.textContent = "Muy brillante: " + brightness;
+  } else {
+    alertText.textContent = "Todo normal: " + brightness;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ let video = document.getElementById("videoInput"); // video is the id of video t
 video.width = jQuery(window).width();
 video.height = jQuery(window).height();
 navigator.mediaDevices
-  .getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
+  .getUserMedia({
+    video: { facingMode: { exact: "environment" } },
+    audio: false
+  })
   .then(function(stream) {
     video.srcObject = stream;
     video.play();
@@ -16,7 +19,6 @@ navigator.mediaDevices
 
     const FPS = 30;
     function processVideo() {
-      
       try {
         // if (!streaming) {
         //   // clean and stop.
@@ -27,6 +29,7 @@ navigator.mediaDevices
         let begin = Date.now();
         // start processing.
         cap.read(src);
+
         //cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
         //cv.imshow("canvasOutput", dst);
         //let src = cv.imread('videoInput');
@@ -45,19 +48,22 @@ navigator.mediaDevices
         cv.calcHist(srcVec, channels, mask, hist, histSize, ranges, accumulate);
         let result = cv.minMaxLoc(hist, mask);
         let max = result.maxVal;
-        let dst = new cv.Mat.zeros(src.rows, histSize[0] * scale,
-                           cv.CV_8UC3);
+        let dst = new cv.Mat.zeros(src.rows, histSize[0] * scale, cv.CV_8UC3);
         // draw histogram
         for (let i = 0; i < histSize[0]; i++) {
-          let binVal = hist.data32F[i] * src.rows / max;
+          let binVal = (hist.data32F[i] * src.rows) / max;
           let point1 = new cv.Point(i * scale, src.rows - 1);
           let point2 = new cv.Point((i + 1) * scale - 1, src.rows - binVal);
           cv.rectangle(dst, point1, point2, color, cv.FILLED);
         }
-        cv.imshow('canvasOutput', dst);
-        src.delete(); dst.delete(); srcVec.delete(); mask.delete(); hist.delete();
+        cv.imshow("canvasOutput", dst);
+        src.delete();
+        dst.delete();
+        srcVec.delete();
+        mask.delete();
+        hist.delete();
 
-          // schedule the next one.
+        // schedule the next one.
         let delay = 1000 / FPS - (Date.now() - begin);
         setTimeout(processVideo, delay);
       } catch (err) {
