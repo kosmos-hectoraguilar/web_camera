f3081becb193c5ee7778651f1c1a61ba35fdfabb
Hector Efren Aguilar <47463539+kosmos-hectoraguilar@users.noreply.github.com>
2019-03-22 16:43:00 -0600

    Se agrega captura de fotografía.

--- src/camera.js
+++ src/camera.js
@@ -1,12 +1,29 @@
 import cv from "opencv.js";
 
-const FPS = 10;
+const FPS = 3;
+const LOW_BRIGHTNESS_LIMIT = 80;
+const HIGH_BRIGHTNESS_LIMIT = 200;
 
 let video = document.getElementById("videoInput"); // video is the id of video tag
 video.width = jQuery(window).width();
 video.height = jQuery(window).height();
 let buttonDiv = document.getElementById("buttonDiv");
 let alertText = document.getElementById("alert");
+var image = document.querySelector("#snap");
+var snapButton = document.querySelector("#screenshot-button");
+
+snapButton.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  var snap = takeSnapshot();
+
+  // Show image.
+  image.setAttribute("src", snap);
+  image.classList.add("visible");
+
+  // Pause video playback of stream.
+  video.pause();
+});
 
 buttonDiv.height = 1200;
 
@@ -19,7 +36,6 @@ navigator.mediaDevices
     video.srcObject = stream;
     video.play();
 
-    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let cap = new cv.VideoCapture(video);
 
     let { width, height } = stream.getTracks()[0].getSettings();
@@ -27,10 +43,10 @@ navigator.mediaDevices
     console.log(`${width}x${height}`);
 
     function processVideo() {
+      //let begin = Date.now();
+
       try {
-        let begin = Date.now();
-        //val pointMatHsv = Mat(1, 1, CvType.CV_8UC3, hsvColor)
-        // Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4)
+        let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         cap.read(src);
 
         let reducedMap = new cv.Mat(480, 360, cv.CV_8UC4);
@@ -42,25 +58,53 @@ navigator.mediaDevices
           0,
           cv.INTER_CUBIC // interpolation method
         );
+        src.delete();
+
         let brightness = calculateBrightness(reducedMap, pointCount);
         reducedMap.delete();
         notifyBrightness(brightness);
-
-        //cv.cvtColor(src, src, cv.COLOR_RGB2HLS, 0);
       } catch (err) {
         console.log("An error occurred! " + err);
       } finally {
-        let delay = 300; //1000 / FPS - (Date.now() - begin);
-        setTimeout(processVideo, delay);
+        //setTimeout(processVideo, delay);
       }
     }
-
-    setTimeout(processVideo, 0);
+    let delay = 1000 / FPS;
+    setInterval(processVideo, delay);
   })
   .catch(function(err) {
     console.log("An error occurred! " + err);
   });
 
+function takeSnapshot() {
+  // Here we're using a trick that involves a hidden canvas element.
+  var hidden_canvas = document.querySelector("canvas"),
+    context = hidden_canvas.getContext("2d");
+
+  var width = video.videoWidth,
+    height = video.videoHeight;
+
+  if (width && height) {
+    // Setup a canvas with the same dimensions as the video.
+    hidden_canvas.width = width;
+    hidden_canvas.height = height;
+
+    // Make a copy of the current frame in the video on the canvas.
+    context.drawImage(video, 0, 0, width, height);
+
+    // Turn the canvas image into a dataURL that can be used as a src for our photo.
+    return hidden_canvas.toDataURL("image/png");
+  }
+}
+
+/**
+ * Convert the RGB matrix into a HLS one, move into all rows and cols and sum the L component of each
+ * element. After it divides the brightness value and pointCount value.
+ *
+ * @param {Mat} src Matrix that has a definition of the frame to analyze
+ * @param {Number} pointCount It is the value of width times height
+ * @returns
+ */
 function calculateBrightness(src, pointCount) {
   let dst = new cv.Mat();
   let brightness = 0;
@@ -74,15 +118,19 @@ function calculateBrightness(src, pointCount) {
   }
   dst.delete();
 
-  //brightness = brightness / pointCount;
-  brightness = brightness / pointCount; //(480 * 360);
+  brightness = brightness / pointCount;
   return brightness;
 }
 
+/**
+ * Show a message in view depending on the brightness value.
+ * @param {Number} brightness Brightness value from a HLS definition, it is between 0 to 254 being darker when the value
+ * reach 0 and brighter when value is high.
+ */
 function notifyBrightness(brightness) {
-  if (brightness < 80) {
+  if (brightness < LOW_BRIGHTNESS_LIMIT) {
     alertText.textContent = "Muy obscuro: " + brightness;
-  } else if (brightness > 200) {
+  } else if (brightness > HIGH_BRIGHTNESS_LIMIT) {
     alertText.textContent = "Muy brillante: " + brightness;
   } else {
     alertText.textContent = "Todo normal: " + brightness;
